2e847c16a6ef29c85cb3e73412393fa10b4f27b6
中野雄貴 <totoro.windows@outlook.com>
2019-11-25 00:30:29 +0900

    迷路を２次元配列から１次元で管理 探索プログラムを追加 エラー処理とか全くしてないからそのうち詰む

--- Maze.py
+++ Maze.py
@@ -1,108 +1,164 @@
+# -*- coding: utf-8 -*-
 import random
+import math
 
 
 # 迷路クラス
 class Maze:
-
     maze_data = []
     maze_x = 0
-    maze_y = 0
+    # maze_y = 0
 
     actor_position = [
-        ['参加者名', '0: 逃げる, 1:　追う', ['x座標', 'y座標']]
+        ['参加者名', '1: 逃げる, 2:　追う', '座標']
     ]
 
     # 初期化
-    # 迷路データを渡す
-    def __init__(self, maze_data):
+    def __init__(self, maze_data, maze_len):
         self.maze_data = maze_data
-        self.maze_x = len(maze_data)
-        self.maze_y = len(maze_data[0])
+        self.maze_x = maze_len
+        # self.maze_y = maze_y
         self.actor_position = []
+
+    # 迷路情報を取得
+    def getInfoMaze(self):
+        return [self.maze_data, self.maze_x]
+
+    # 参加者を取得
+    def getActor(self):
+        return self.actor_position
 
     # 参加者セット
     def setActor(self, name, key, position):
         self.actor_position.append([name, key, position])
 
     # 移動可能か
-    def isCanMove(self, x, y):
+    def isCanMove(self, p):
         try:
-            if x < 0 or y < 0:
+            if p < 1:
                 return False
-            if(self.maze_data[y][x] == 0):
+            if (self.maze_data[p - 1] == 0):
                 return True
         except:
             return False
         return False
 
-    # Actorを(0,0)とした時の迷路座標との変換
-    def getConvertPosition(self, act_position):
-        abs_position = []
-        for i in range(1 - int(self.maze_x/2) + 1, int(self.maze_x/2) + 1):
-            for j in range(1 - int(self.maze_y / 2) + 1, int(self.maze_y / 2) + 1):
-                # if i == 0 and j == 0:
-                #     abs_position.append([None, None])
-                if self.isCanMove(act_position[0] + j, act_position[1] + i):
-                    abs_position.append(
-                        [act_position[0] + j, act_position[1] + i]
-                    )
-                else:
-                    abs_position.append([None, None])
-        return abs_position
+    # 追跡者の探索範囲にtargetがいるか
+    # いれば座標をいなければnoneを返す
+    def searchTarget(self, position):
+        # 列数
+        column = self.getInfoMaze()[1]
 
-    # 追跡者の探索範囲に逃げてるやつがいるか
-    # いれば座標をいなければnoneを返却
-    def searchTarget(self):
-        count = 0
-        relative_chaser = self.getConvertPosition(
-            self.actor_position[0][2])
-        absolute_chaser = self.actor_position[0][2]
+        # 追跡者の点p
+        p = position
 
-        relative_maze_num = relative_chaser.index(absolute_chaser)
-        maze_x_half = self.maze_x % relative_maze_num
+        # 追跡者のいる行
+        act_row = int(math.ceil(p / column))
 
-        for i in range():
-            pass
+        '''
+        方向
+        5: 左
+        6: 右
+        7: 下
+        8: 上
+        '''
 
-        return maze_x_half
-        # return count
+        # 横方向(マイナス)
+        # 移動可能マス
+        available_square = []
+        for i in range(p - 1, column * (act_row - 1), -1):
+            # 移動可能か
+            if self.isCanMove(i):
+                available_square.append(i)
+            else:
+                break
+        # 探索範囲にいるか
+        for i in available_square:
+            if self.actor_position[1][2] == i:
+                return i, 5
+
+        # 横方向(プラス)
+        # 移動可能マス
+        available_square = []
+        for i in range(p + 1, column * act_row):
+            # 移動可能か
+            if self.isCanMove(i):
+                available_square.append(i)
+            else:
+                break
+        # 探索範囲にいるか
+        for i in available_square:
+            if self.actor_position[1][2] == i:
+                return i, 6
+
+        # 縦方向(マイナス)
+        # 移動可能マス
+        available_square = []
+        for i in range(-1, -column, -1):
+            # 移動可能か
+            if self.isCanMove(p - column * i):
+                available_square.append(p - column * i)
+            else:
+                break
+        # 探索範囲にいるか
+        for i in available_square:
+            if self.actor_position[1][2] == i:
+                return i, 7
+
+        # 縦方向(プラス)
+        # 移動可能マス
+        available_square = []
+        for i in range(1, column):
+            # 移動可能か
+            if self.isCanMove(p + column * i):
+                available_square.append(p + column * i)
+            else:
+                break
+        # 探索範囲にいるか
+        for i in available_square:
+            if self.actor_position[1][2] == i:
+                return i, 8
+
+        return False
 
 
 # 人
-class Actor():
+class Actor:
     # 場所
-    position = []
+    position = None
     maze_connector = None
     name = None
 
-    #
+    # 初期化
     def __init__(self, maze_connector, name, key):
         self.maze_connector = maze_connector
-        initial_position = self.generateInitialPosition()
-        self.position = [initial_position[0], initial_position[1]]
+        self.position = self.generateInitialPosition()
         self.maze_connector.setActor(name, key, self.position)
         self.name = name
 
     # 初期位置生成
     def generateInitialPosition(self):
-        while (True):
-            _x = random.randrange(self.maze_connector.maze_x)
-            _y = random.randrange(self.maze_connector.maze_x)
-            if self.maze_connector.isCanMove(_x, _y):
+        while True:
+            p = random.randrange(
+                int(len(self.maze_connector.getInfoMaze()[0])))
+            if self.maze_connector.isCanMove(p):
                 break
-        return (_x, _y)
+        return p
 
     # 移動する
     def moveTo(self, position):
-        # 移動できるか
-        if self.maze_connector.isCanMove(position[0], position[1]):
+        # 移動可能か
+        if self.maze_connector.isCanMove(position):
             self.position = position
+            return True
+        return False
 
 
-# 逃げる人
+# 逃げるやつ
 class Target(Actor):
+    # 逃げるやつを設定
     def __init__(self, maze_connector, name):
-        super().__init__(maze_connector, name, 0)
+        super().__init__(maze_connector, name, 1)
 
 
 # 追跡者
@@ -110,46 +166,56 @@ class Chaser(Actor):
     # 見つけたflag
     flag = False
 
+    # 追跡者を設定
     def __init__(self, maze_connector, name):
-        super().__init__(maze_connector, name, 1)
+        super().__init__(maze_connector, name, 2)
 
-    # フラグ管理
+    # フラグをセット
     def setFlag(self, flag):
         self.flag = flag
 
+    # フラグをゲット
+    def getFlag(self):
+        return self.flag
+
     # 探索
     def searchTarget(self):
-        seach_range = self.maze_connector.getConvertPosition(self.name)
-        pass
+        search_result = self.maze_connector.searchTarget(self.position)
+        if not search_result:
+            # 見つからなかった時
+            target_p = None
+            direction = 100
+            pass
+        else:
+            # ターゲットの位置
+            target_p = search_result[0]
+            # ターゲットの方向
+            direction = search_result[1]
 
-    # 探索可能範囲の取得
-    def getSearchRange(self):
-        _local_x = self.position[0]
-        _local_y = self.position[1]
-        maze_data = self.maze_connector
-
-        pass
+        return target_p, direction
 
 
 if __name__ == "__main__":
     data = [
-        [0, 0, 0, 0, 0, 1, 1, 0, 0, 0],
-        [0, 1, 1, 0, 0, 0, 1, 0, 0, 0],
-        [0, 1, 1, 0, 1, 0, 1, 0, 0, 0],
-        [0, 0, 0, 0, 1, 0, 0, 0, 1, 0],
-        [0, 0, 0, 0, 1, 0, 0, 0, 1, 0],
-        [0, 1, 1, 0, 1, 0, 1, 1, 0, 0],
-        [0, 0, 0, 0, 1, 0, 0, 1, 0, 1],
-        [0, 0, 0, 0, 1, 0, 0, 0, 0, 1]
+        0, 0, 0, 0, 0, 1, 1, 0, 0, 0,
+        0, 1, 1, 0, 0, 0, 1, 0, 0, 0,
+        0, 1, 1, 0, 1, 0, 1, 0, 0, 0,
+        0, 0, 0, 0, 1, 0, 0, 0, 1, 0,
+        0, 0, 0, 0, 1, 0, 0, 0, 1, 0,
+        0, 1, 1, 0, 1, 0, 1, 1, 0, 0,
+        0, 0, 0, 0, 1, 0, 0, 1, 0, 1,
+        0, 0, 0, 0, 1, 0, 0, 0, 0, 1,
+        0, 0, 0, 0, 1, 0, 0, 1, 0, 1,
+        0, 0, 0, 0, 1, 0, 0, 0, 0, 1
     ]
-    maze = Maze(data)
+    maze = Maze(data, int(math.sqrt(len(data))))
 
     # print(maze.isCanMove(-2, -2))
     chre = Chaser(maze, 'chaser')
     user = Target(maze, 'target')
 
     print(maze.actor_position)
-    print(maze.searchTarget())
+    print(maze.searchTarget(chre.position))
 
     # user
     # print(user.position)
---
+++ old_maze.py
@@ -0,0 +1,167 @@
+# # -*- coding: utf-8 -*-
+# import random
+
+
+# # 迷路クラス
+# class Maze:
+
+#     maze_data = []
+#     maze_x = 0
+#     maze_y = 0
+
+#     actor_position = [
+#         ['参加者名', '0: 逃げる, 1:　追う', ['x座標', 'y座標']]
+#     ]
+
+#     # 初期化
+#     # 迷路データを渡す
+#     def __init__(self, maze_data):
+#         self.maze_data = maze_data
+#         self.maze_x = len(maze_data)
+#         self.maze_y = len(maze_data[0])
+#         self.actor_position = []
+
+#     # 参加者セット
+#     def setActor(self, name, key, position):
+#         self.actor_position.append([name, key, position])
+
+#     # 移動可能か
+#     def isCanMove(self, x, y):
+#         try:
+#             if x < 0 or y < 0:
+#                 return False
+#             if(self.maze_data[y][x] == 0):
+#                 return True
+#         except:
+#             return False
+#         return False
+
+#     # Actorを(0,0)とした時の迷路座標との変換
+#     def getConvertPosition(self, act_position):
+#         abs_position = []
+#         for i in range(1 - int(self.maze_x/2) + 1, int(self.maze_x/2) + 1):
+#             for j in range(1 - int(self.maze_y / 2) + 1, int(self.maze_y / 2) + 1):
+#                 # if i == 0 and j == 0:
+#                 #     abs_position.append([None, None])
+#                 if self.isCanMove(act_position[0] + j, act_position[1] + i):
+#                     abs_position.append(
+#                         [act_position[0] + j, act_position[1] + i]
+#                     )
+#                 else:
+#                     abs_position.append([None, None])
+#         return abs_position
+
+#     # 追跡者の探索範囲に逃げてるやつがいるか
+#     # いれば座標をいなければnoneを返却
+#     def searchTarget(self):
+#         count = 0
+#         relative_chaser = self.getConvertPosition(
+#             self.actor_position[0][2])
+#         absolute_chaser = self.actor_position[0][2]
+
+#         relative_maze_num = relative_chaser.index(absolute_chaser)
+#         maze_x_half = self.maze_x % relative_maze_num
+
+#         available_x = False
+#         available_y = False
+
+#         # 横方向
+#         for i in range(self.maze_x / 2 + 1):
+#             if not available_x:
+#                 # self.isCanMove()
+#                 pass
+#             pass
+
+#         for i in range(self.maze_y/2 + 1):
+#             pass
+
+#         return maze_x_half
+#         # return count
+
+
+# # 人
+# class Actor:
+#     # 場所
+#     position = []
+#     maze_connector = None
+#     name = None
+
+#     #
+#     def __init__(self, maze_connector, name, key):
+#         self.maze_connector = maze_connector
+#         initial_position = self.generateInitialPosition()
+#         self.position = [initial_position[0], initial_position[1]]
+#         self.maze_connector.setActor(name, key, self.position)
+#         self.name = name
+
+#     # 初期位置生成
+#     def generateInitialPosition(self):
+#         while (True):
+#             _x = random.randrange(self.maze_connector.maze_x)
+#             _y = random.randrange(self.maze_connector.maze_x)
+#             if self.maze_connector.isCanMove(_x, _y):
+#                 break
+#         return (_x, _y)
+
+#     # 移動する
+#     def moveTo(self, position):
+#         # 移動できるか
+#         if self.maze_connector.isCanMove(position[0], position[1]):
+#             self.position = position
+
+
+# # 逃げる人
+# class Target(Actor):
+#     def __init__(self, maze_connector, name):
+#         super().__init__(maze_connector, name, 0)
+
+
+# # 追跡者
+# class Chaser(Actor):
+#     # 見つけたflag
+#     flag = False
+
+#     def __init__(self, maze_connector, name):
+#         super().__init__(maze_connector, name, 1)
+
+#     # フラグ管理
+#     def setFlag(self, flag):
+#         self.flag = flag
+
+#     # 探索
+#     def searchTarget(self):
+#         seach_range = self.maze_connector.getConvertPosition(self.name)
+#         pass
+
+#     # 探索可能範囲の取得
+#     def getSearchRange(self):
+#         _local_x = self.position[0]
+#         _local_y = self.position[1]
+#         maze_data = self.maze_connector
+
+#         pass
+
+
+# if __name__ == "__main__":
+#     data = [
+#         [0, 0, 0, 0, 0, 1, 1, 0, 0, 0],
+#         [0, 1, 1, 0, 0, 0, 1, 0, 0, 0],
+#         [0, 1, 1, 0, 1, 0, 1, 0, 0, 0],
+#         [0, 0, 0, 0, 1, 0, 0, 0, 1, 0],
+#         [0, 0, 0, 0, 1, 0, 0, 0, 1, 0],
+#         [0, 1, 1, 0, 1, 0, 1, 1, 0, 0],
+#         [0, 0, 0, 0, 1, 0, 0, 1, 0, 1],
+#         [0, 0, 0, 0, 1, 0, 0, 0, 0, 1]
+#     ]
+#     maze = Maze(data)
+
+#     # print(maze.isCanMove(-2, -2))
+#     chre = Chaser(maze, 'chaser')
+#     user = Target(maze, 'target')
+
+#     print(maze.actor_position)
+#     print(maze.searchTarget())
+
+#     # user
+#     # print(user.position)
+#     # print(chre.position)
